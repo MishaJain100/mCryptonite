7b6c106d376a960ae0e1b91e76e634fa297efe1e
MishaJain100 <mishajain100@gmail.com>
2024-05-25 15:51:14 +0530

    SSTI Commit

--- PicoCTF/Cryptography/C3.py
+++ PicoCTF/Cryptography/C3.py
@@ -12,20 +12,20 @@ for char in out:
     chars += lookup1[cur]
     prev = cur
 print (chars)
-    
 
-#asciiorder
-#fortychars
-#selfinput
-#pythontwo
 
-chars = ""
-from fileinput import input
-for line in input():
-    chars += line
-b = 1 / 1
+# #asciiorder
+# #fortychars
+# #selfinput
+# #pythontwo
 
-for i in range(len(chars)):
-    if i == b * b * b:
-        print (chars[i]) #prints
-        b += 1 / 1
+# chars = ""
+# from fileinput import input
+# for line in input():
+#     chars += line
+# b = 1 / 1
+
+# for i in range(len(chars)):
+#     if i == b * b * b:
+#         print (chars[i]) #prints
+#         b += 1 / 1
